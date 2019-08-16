c89f42e4a9941c378e0ad3c16a7d66e58824828a
cayoujimole@gmail.com <zc881004>
2019-08-16 15:01:57 +0800

    6

--- src/main/java/com/demo/email/controller/TestThread.java
+++ src/main/java/com/demo/email/controller/TestThread.java
@@ -16,15 +16,20 @@ public class TestThread extends Thread {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(tt);
-        Thread t2 = new Thread(tt);
-        Thread t3 = new TestThread();
-        t1.start();
-        t2.start();
-        t3.start();
-        t1.join();
-        t2.join();
-        System.out.println(i);
+//        Thread t1 = new Thread(tt);
+//        Thread t2 = new Thread(tt);
+//        Thread t3 = new TestThread();
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t1.join();
+//        t2.join();
+//        System.out.println(i);
+        String s1 = "hello world";
+        String s2 = new String("hello world");
+        System.out.println(s1 == s2);
+        s2.intern();
+        System.out.println(s1 == s2);
     }
 
 }
