830e7d0ce6986ba293a19a75acf99e547b3fa70a
cayoujimole@gmail.com <zc881004>
2019-08-16 14:55:44 +0800

    6

--- src/main/java/com/demo/email/controller/TestRunnable.java
+++ src/main/java/com/demo/email/controller/TestRunnable.java
@@ -22,12 +22,7 @@ public class TestRunnable implements Runnable {
         t1.setName("t1");
         t2.setName("t2");
         t3.setName("t3");
-//        t1.start();
-//        t1.join();
-//        t2.start();
-//        t2.join();
-//        t3.start();
-//        t3.join();
+
         t1.setPriority(1);
         t2.setPriority(2);
         t3.setPriority(3);
